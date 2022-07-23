560a8bf0240c16f94fd73f3ff5b141291ee3fb17
Yaswanth Tondepu <yaswanthtondepu@gmail.com>
2022-07-23 11:08:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Component}  from 'react';
+import React  from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import TourList from './components/TourList';
